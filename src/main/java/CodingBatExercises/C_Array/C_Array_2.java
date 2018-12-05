2bb93116754a3adb6f29a998cd56515c56002116
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2018-12-05 17:51:21 +0100

    Array exercises

--- src/main/java/CodingBatExercises/C_Array/C_Array_2.java
+++ src/main/java/CodingBatExercises/C_Array/C_Array_2.java
@@ -1,5 +1,7 @@
 package CodingBatExercises.C_Array;
 
+import java.util.Arrays;
+
 public class C_Array_2 {
     /**
     Return the number of even ints in the given array. Note: the % "mod" operator computes the remainder, e.g. 5 % 2 is 1.
@@ -48,6 +50,160 @@ public class C_Array_2 {
             }
         return true;
     }
+
+    /**
+     Given a number n, create and return a new int array of length n, containing the numbers 0, 1, 2, ... n-1.
+     The given n may be 0, in which case just return a length 0 array. You do not need a separate if-statement for the
+     length-0 case; the for-loop should naturally execute 0 times in that case, so it just works. The syntax to make
+     a new int array is: new int[desired_length]
+         fizzArray(4) --> [0, 1, 2, 3]
+         fizzArray(1) --> [0]
+         fizzArray(10) --> [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+     */
+    public int[] fizzArray(int n) {
+        int[] output = new int[n];
+        for (int i = 0; i < output.length; i++) {
+            output[i] = i;
+        }
+        return output;
+    }
+
+    /**
+     Given an array of ints, return true if it contains no 1's or it contains no 4's.
+         no14([1, 2, 3]) --> true
+         no14([1, 2, 3, 4]) --> false
+         no14([2, 3, 4]) --> true
+     */
+    public boolean no14(int[] nums) {
+        int no1s = 0;
+        int no4s = 0;
+        for (int num : nums) {
+            if (num == 1) no1s++;
+            if (num == 4) no4s++;
+        }
+        return (no1s > 0 ^ no4s > 0);
+    }
+
+    /**
+     Given arrays nums1 and nums2 of the same length, for every element in nums1, consider the corresponding
+     element in nums2 (at the same index). Return the count of the number of times that the two elements
+     differ by 2 or less, but are not equal.
+         matchUp([1, 2, 3], [2, 3, 10]) --> 2
+         matchUp([1, 2, 3], [2, 3, 5]) --> 3
+         matchUp([1, 2, 3], [2, 3, 3]) --> 2
+     */
+    public int matchUp(int[] nums1, int[] nums2) {
+        int counter = 0;
+        for (int i = 0; i < nums1.length; i++) {
+            if ((Math.abs(nums1[i] - nums2[i]) <= 2) && nums1[i] != nums2[i]) counter++;
+        }
+        return counter;
+    }
+
+    /**
+     Given an array of ints, return true if the array contains either 3 even or 3 odd values all next to each other.
+         modThree([2, 1, 3, 5]) --> true
+         modThree([2, 1, 2, 5]) --> false
+         modThree([2, 4, 2, 5]) --> true
+         modThree([2, 1, 2, 1, 4, 8, 10]) --> true
+     */
+    public boolean modThree(int[] nums) {
+        int evenCounter = 0;
+        int oddCounter = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
+                evenCounter++;
+                oddCounter = 0;
+                if (evenCounter == 3){
+                    return true;
+                }
+            }
+            if (nums[i] % 2 != 0) {
+                oddCounter++;
+                evenCounter = 0;
+                if (oddCounter == 3){
+                    return true;
+                }
+            }
+        }
+        return (evenCounter >= 3 ^ oddCounter >= 3);
+    }
+
+    /**
+     Return true if the group of N numbers at the start and end of the array are the same.
+     For example, with {5, 6, 45, 99, 13, 5, 6}, the ends are the same for n=0 and n=2, and false for n=1 and n=3.
+     You may assume that n is in the range 0..nums.length inclusive.
+         sameEnds([5, 6, 45, 99, 13, 5, 6], 1) --> false
+         sameEnds([5, 6, 45, 99, 13, 5, 6], 2) --> true
+         sameEnds([5, 6, 45, 99, 13, 5, 6], 3) --> false
+     */
+    public boolean sameEnds(int[] nums, int len) {
+        return Arrays.equals(
+                Arrays.copyOfRange(nums, 0, len),
+                Arrays.copyOfRange(nums, nums.length - len, nums.length));
+    }
+
+    /**
+     Return an array that is "left shifted" by one -- so {6, 2, 5, 3} returns {2, 5, 3, 6}.
+     You may modify and return the given array, or return a new array.
+         shiftLeft([6, 2, 5, 3]) --> [2, 5, 3, 6]
+         shiftLeft([1, 2]) --> [2, 1]
+         shiftLeft([1]) --> [1]
+     */
+    public int[] shiftLeft(int[] nums) {
+        int[] output = new int[nums.length];
+        for (int i = 0; i < output.length; i++) {
+            if (i == output.length - 1){
+                output[i] = nums[0];
+            }else {
+                output[i] = nums[i + 1];
+            }
+        }
+        return output;
+    }
+
+    /**
+     Given a non-empty array of ints, return a new array containing the elements from the original array that come
+     after the last 4 in the original array. The original array will contain at least one 4.
+     Note that it is valid in java to create an array of length 0.
+         post4([2, 4, 1, 2]) --> [1, 2]
+         post4([4, 1, 4, 2]) --> [2]
+         post4([4, 4, 1, 2, 3]) --> [1, 2, 3]
+     */
+    public int[] post4(int[] nums) {
+        int last4Index = 0;
+        int i = 0;
+        for (i = 0; i < nums.length; i++) {
+            if (nums[i] == 4) last4Index = i;
+        }
+        last4Index++;
+        int[] output = new int[nums.length - last4Index];
+        for (int j = 0; j < output.length; j++) {
+            output[j] = nums[last4Index++];
+        }
+        return output;
+    }
+
+    /**
+     Return a version of the given array where all the 10's have been removed. The remaining elements should shift left
+     towards the start of the array as needed, and the empty spaces a the end of the array should be 0.
+     So {1, 10, 10, 2} yields {1, 2, 0, 0}. You may modify and return the given array or make a new array.
+         withoutTen([1, 10, 10, 2]) --> [1, 2, 0, 0]
+         withoutTen([10, 2, 10]) --> [2, 0, 0]
+         withoutTen([1, 99, 10]) --> [1, 99, 0]
+     */
+    public int[] withoutTen(int[] nums) {
+        int[] output = new int[nums.length];
+        int i = 0;
+        for (int num : nums){
+            if (num != 10){
+                output[i] = num;
+                i++;
+            }
+        }
+        return output;
+    }
+
 
 
 }
---
+++ src/test/java/CodingBatExercises/C_Array_TEST/C_Array_2_Test.java
@@ -0,0 +1,274 @@
+package CodingBatExercises.C_Array_TEST;
+
+import CodingBatExercises.C_Array.C_Array_2;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class C_Array_2_Test {
+
+    @Test
+    public void TEST_countEvens() {
+        C_Array_2 c_array_2_tester = new C_Array_2();
+        int[] nums1 = {2,1,2,3,4};
+        int[] nums2 = {2,2,0};
+        int[] nums3 = {1,3,5};
+
+        assertEquals(3, c_array_2_tester.countEvens(nums1));
+        assertEquals(3, c_array_2_tester.countEvens(nums2));
+        assertEquals(0, c_array_2_tester.countEvens(nums3));
+    }
+
+    @Test
+    public void TEST_sum13(){
+        C_Array_2 c_array_2_tester = new C_Array_2();
+
+        int[] nums1 = {1,2,2,1};
+        int[] nums2 = {1,1};
+        int[] nums3 = {1,2,2,1,13};
+        int[] nums4 = {1,13,2,1};
+
+        assertEquals(6, c_array_2_tester.sum13(nums1));
+        assertEquals(2, c_array_2_tester.sum13(nums2));
+        assertEquals(6, c_array_2_tester.sum13(nums3));
+        assertEquals(2, c_array_2_tester.sum13(nums4));
+    }
+
+    @Test
+    public void TEST_lucky13(){
+        C_Array_2 c_array_2_tester = new C_Array_2();
+
+        int[] nums1 = {0,2,4};
+        int[] nums2 = {1,2,3};
+        int[] nums3 = {1,2,4};
+
+        assertTrue(c_array_2_tester.lucky13(nums1));
+        assertFalse(c_array_2_tester.lucky13(nums2));
+        assertFalse(c_array_2_tester.lucky13(nums3));
+    }
+
+    @Test
+    public void TEST_fizzArray(){
+        C_Array_2 c_array_2_tester = new C_Array_2();
+
+        int[] nums1 = new int[]{0,1,2,3};
+        int[] nums2 = new int[]{0};
+        int[] nums3 = new int[]{0,1,2,3,4,5,6,7,8,9};
+
+        assertArrayEquals(nums1, c_array_2_tester.fizzArray(4));
+        assertArrayEquals(nums2, c_array_2_tester.fizzArray(1));
+        assertArrayEquals(nums3, c_array_2_tester.fizzArray(10));
+    }
+
+    @Test
+    public void TEST_no14(){
+        C_Array_2 c_array_2_tester = new C_Array_2();
+
+        int[] nums1 = new int[]{1,2,3};
+        int[] nums2 = new int[]{1,2,3,4};
+        int[] nums3 = new int[]{2,3,4};
+
+        assertTrue(c_array_2_tester.no14(nums1));
+        assertFalse(c_array_2_tester.no14(nums2));
+        assertTrue(c_array_2_tester.no14(nums3));
+    }
+
+    @Test
+    public void TEST_matchUp(){
+        C_Array_2 c_array_2_tester = new C_Array_2();
+
+        int[] nums1 = new int[]{1,2,3};
+        int[] nums2 = new int[]{2,3,10};
+        int[] nums3 = new int[]{1,2,3};
+        int[] nums4 = new int[]{2,3,5};
+        int[] nums5 = new int[]{1,2,3};
+        int[] nums6 = new int[]{2,3,3};
+
+        assertEquals(2, c_array_2_tester.matchUp(nums1, nums2));
+        assertEquals(3, c_array_2_tester.matchUp(nums3, nums4));
+        assertEquals(2, c_array_2_tester.matchUp(nums5, nums6));
+    }
+
+    @Test
+    public void TEST_modThree() {
+        C_Array_2 c_array_2_tester = new C_Array_2();
+
+        int[] nums1 = new int[]{2, 1, 3, 5};
+        int[] nums2 = new int[]{2, 1, 2, 5};
+        int[] nums3 = new int[]{2, 4, 2, 5};
+        int[] nums4 = new int[]{2, 1, 2, 1, 4, 8, 10};
+
+        assertTrue(c_array_2_tester.modThree(nums1));
+        assertFalse(c_array_2_tester.modThree(nums2));
+        assertTrue(c_array_2_tester.modThree(nums3));
+        assertTrue(c_array_2_tester.modThree(nums4));
+    }
+
+    @Test
+    public void TEST_sameEnds() {
+        C_Array_2 c_array_2_tester = new C_Array_2();
+
+        int[] nums = new int[]{5, 6, 45, 99, 13, 5, 6};
+
+        assertFalse(c_array_2_tester.sameEnds(nums, 1));
+        assertTrue(c_array_2_tester.sameEnds(nums, 2));
+        assertFalse(c_array_2_tester.sameEnds(nums, 3));
+    }
+
+    @Test
+    public void TEST_shiftLeft() {
+        C_Array_2 c_array_2_tester = new C_Array_2();
+
+        int[] nums1 = new int[]{6, 2, 5, 3};
+        int[] nums2 = new int[]{1, 2};
+        int[] nums3 = new int[]{1};
+
+        assertArrayEquals(new int[]{2, 5, 3, 6}, c_array_2_tester.shiftLeft(nums1));
+        assertArrayEquals(new int[]{2,1}, c_array_2_tester.shiftLeft(nums2));
+        assertArrayEquals(new int[]{1}, c_array_2_tester.shiftLeft(nums3));
+    }
+
+    @Test
+    public void TEST_post4() {
+        C_Array_2 c_array_2_tester = new C_Array_2();
+
+        int[] nums1 = new int[]{2,4,1,2};
+        int[] nums2 = new int[]{4,1,4,2};
+        int[] nums3 = new int[]{4,4,1,2,3};
+
+        assertArrayEquals(new int[]{1,2}, c_array_2_tester.post4(nums1));
+        assertArrayEquals(new int[]{2}, c_array_2_tester.post4(nums2));
+        assertArrayEquals(new int[]{1,2,3}, c_array_2_tester.post4(nums3));
+    }
+
+    @Test
+    public void TEST_withoutTen() {
+        C_Array_2 c_array_2_tester = new C_Array_2();
+
+        int[] nums1 = new int[]{1,10,10,2};
+        int[] nums2 = new int[]{10,2,10};
+        int[] nums3 = new int[]{1,99,10};
+
+        assertArrayEquals(new int[]{1,2,0,0}, c_array_2_tester.withoutTen(nums1));
+        assertArrayEquals(new int[]{2,0,0}, c_array_2_tester.withoutTen(nums2));
+        assertArrayEquals(new int[]{1,99,0}, c_array_2_tester.withoutTen(nums3));
+    }
+
+//    @Test
+//    public void TEST_fizzBuzz() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_bigDiff() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_sum67() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_sum28() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_only14() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_isEverywhere() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_has77() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_haveThree() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_tripleUp() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_tenRun() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_notAlone() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_zeroMax() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_centeredAverage() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_has22() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_more14() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_fizzArray2() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_either24() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_has12() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_twoTwo() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_fizzArray3() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_pre4() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_zeroFront() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+//
+//    @Test
+//    public void TEST_evenOdd() {
+//        C_Array_2 c_array_2_tester = new C_Array_2();
+//    }
+}
