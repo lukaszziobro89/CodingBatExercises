0249af51670cbbd18f00832afc1a33d9694deb3b
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2018-12-21 22:06:47 +0100

    Java streams exercises

--- src/main/java/CodingBatExercises/E_Functional/E_Functional_1.java
+++ src/main/java/CodingBatExercises/E_Functional/E_Functional_1.java
@@ -1,4 +1,18 @@
 package CodingBatExercises.E_Functional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class E_Functional_1 {
+    /**
+     Given a list of integers, return a list where each integer is multiplied by 2.
+         doubling([1, 2, 3]) → [2, 4, 6]
+         doubling([6, 8, 6, 8, -1]) → [12, 16, 12, 16, -2]
+         doubling([]) → []
+     */
+    public List<Integer> doubling(List<Integer> nums) {
+        return nums.stream()
+                .map(n -> n * 2)
+                .collect(Collectors.toList());
+    }
 }
--- src/test/java/CodingBatExercises/E_Functional_TEST/E_Functional_1_Test.java
+++ src/test/java/CodingBatExercises/E_Functional_TEST/E_Functional_1_Test.java
@@ -1,4 +1,19 @@
 package CodingBatExercises.E_Functional_TEST;
 
+import CodingBatExercises.E_Functional.E_Functional_1;
+import org.junit.Test;
+import java.util.Arrays;
+import java.util.Collections;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class E_Functional_1_Test {
+
+    private E_Functional_1 e_functional_1 = new E_Functional_1();
+
+    @Test
+    public void TEST_doubling(){
+        assertEquals(Arrays.asList(2,4,6), e_functional_1.doubling(Arrays.asList(1,2,3)));
+        assertEquals(Arrays.asList(12,16,12,16,-2), e_functional_1.doubling(Arrays.asList(6,8,6,8,-1)));
+        assertEquals(Collections.emptyList(), e_functional_1.doubling(Collections.emptyList()));
+    }
 }
