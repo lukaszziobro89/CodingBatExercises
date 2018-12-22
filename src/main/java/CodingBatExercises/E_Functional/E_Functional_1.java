abdc8e4e4a9711ac07c42d9ef112bf3bf0b96b90
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2018-12-22 09:17:33 +0100

    Java streams exercises

--- src/main/java/CodingBatExercises/E_Functional/E_Functional_1.java
+++ src/main/java/CodingBatExercises/E_Functional/E_Functional_1.java
@@ -28,4 +28,17 @@ public class E_Functional_1 {
                 .collect(Collectors.toList());
     }
 
+    /**
+    Given a list of non-negative integers, return an integer list of the rightmost digits. (Note: use %)
+        rightDigit([1, 22, 93]) → [1, 2, 3]
+        rightDigit([16, 8, 886, 8, 1]) → [6, 8, 6, 8, 1]
+        rightDigit([10, 0]) → [0, 0]
+     */
+    public List<Integer> rightDigit(List<Integer> nums) {
+        return nums.stream()
+                .map(num -> num % 10)
+                .collect(Collectors.toList());
+    }
+
+
 }
--- src/test/java/CodingBatExercises/E_Functional_TEST/E_Functional_1_Test.java
+++ src/test/java/CodingBatExercises/E_Functional_TEST/E_Functional_1_Test.java
@@ -23,4 +23,11 @@ public class E_Functional_1_Test {
         assertEquals(Arrays.asList("242424", "aaa", ""), e_functional_1.copies3(Arrays.asList("24", "a", "")));
         assertEquals(Arrays.asList("hellohellohello", "theretherethere"), e_functional_1.copies3(Arrays.asList("hello", "there")));
     }
+
+    @Test
+    public void TEST_rightDigit(){
+        assertEquals(Arrays.asList(1,2,3), e_functional_1.rightDigit(Arrays.asList(1, 22, 93)));
+        assertEquals(Arrays.asList(6,8,6,8,1), e_functional_1.rightDigit(Arrays.asList(16, 8, 886, 8, 1)));
+        assertEquals(Arrays.asList(0,0), e_functional_1.rightDigit(Arrays.asList(10, 0)));
+    }
 }
